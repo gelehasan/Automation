17a7faec1bdc06ed6759c7895842a0f17de40ef1
Gele <gellehussen@gmail.com>
2025-04-20 18:56:34 +0200

    first intial

---
+++ Brute-Force 2fa.py
@@ -0,0 +1,26 @@
+import requests
+
+
+login_url="example.com"
+otp_url="example/mfa.com"
+dashboard_url="example/dashboard.com"
+
+
+credentials={
+    "username":"username123",
+    "password":"text1234"
+}
+
+
+
+header={
+    'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'http://mfa.thm',
+    'Connection': 'close',
+    'Referer': 'http://mfa.thm/labs/third/mfa',
+    'Upgrade-Insecure-Requests': '1'
+}
